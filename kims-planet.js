34d18e111f190565f7ed9564ffabdf0e106be4b7
kimchampion <kimchampion.mobile@gmail.com>
2025-12-11 09:40:14 -0800

    updated button

--- kims-planet.js
+++ kims-planet.js
@@ -61,42 +61,9 @@ function setup() {
     });
   }
 
-  //back button
-  let base = min(width, height);
-  let pad   = base * 0.05;   
-  let butW  = width * .9;    
-  let bx = width - butW - pad;
-
-  let bttn = createButton("Back");
-  bttn.position(bx,pad);
-
-  bttn.position(20, 20);                  
-  bttn.style("padding", "10px 22px");
-  bttn.style("font-size", "20px");
-  bttn.style("background", "#222");
-  bttn.style("color", "white");
-  bttn.style("border", "2px solid #555");
-  bttn.style("border-radius", "8px");
-  bttn.style("cursor", "pointer");
-  bttn.style("z-index", "9999");
-  bttn.style("transition", "all 0.2s ease");
-  bttn.style("font-family", "spaceFont"); // CSS family name, optional
-
-  // Hover effect for Back button
-  bttn.mouseOver(() => {
-    bttn.style("background", "#444");
-    bttn.style("transform", "scale(1.1)");
-    bttn.style("border", "2px solid white");
-  });
-
-  bttn.mouseOut(() => {
-    bttn.style("background", "#222");
-    bttn.style("transform", "scale(1.0)");
-    bttn.style("border", "2px solid #555");
-  });
-
-
-   bttn.mousePressed(redirect);
+  //x,y,buttonText, redirect
+  let bttn = new Button(20,20,"Back","./");
+  bttn.show();
 }
 
 function windowResized() {
@@ -446,10 +413,6 @@ function drawInfoBox() {
     text(infoLines[i], textX, textY + i * lineH);
   }
   pop();
-}
-
-function redirect(){
-  window.location.href = "./"; // go back to the landing page
 }
 
 function stopAllObjectSounds() {
